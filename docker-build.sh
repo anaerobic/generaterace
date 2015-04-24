7de5fc066788b735a523049da5e83581468a069e
mwmartin <mwm.cal@gmail.com>
2015-04-23 22:27:01 -0600

    fixing docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,12 @@
-/bin/sh
+#!/bin/bash
+ 
+if [[ $1 != "" ]]
+then
+        wget -c "$1";
+        sudo shutdown -k 0;
+else
+        echo "Usage: sudo ./downloadAndShutdown <fileToDownload>";
+fi
 
 git pull origin
 
