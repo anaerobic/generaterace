b66a220289d58e021bbbd84224778114f3280eac
Michael Martin <mwm.cal@gmail.com>
2015-05-19 12:25:56 -0700

    fixing race5kx5.cmd

--- race5kx5.cmd
+++ race5kx5.cmd
@@ -1,9 +1,9 @@
 mkdir data
 
-GenerateRace\bin\Debug\GenerateRace.exe 5000 5 > data\race5kx5.json
+GenerateRace\GenerateRace\bin\Debug\GenerateRace.exe 5000 5 > data\race5kx5.json
 
-StreamingFSharp\bin\Debug\StreamingFSharp.exe data\race5kx5.json > data\race5kx5results-fsharp.json
+StreamingFSharp\StreamingFSharp\bin\Debug\StreamingFSharp.exe data\race5kx5.json > data\race5kx5results-fsharp.json
 
-StreamingCSharp\bin\Debug\StreamingCSharp.exe data\race5kx5.json > data\race5kx5results-csharp.json
+StreamingCSharp\StreamingCSharp\bin\Debug\StreamingCSharp.exe data\race5kx5.json > data\race5kx5results-csharp.json
 
 type data\race5kx5.json | node StreamingTypeScript\main.js > data\race5kx5results-node.json
